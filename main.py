9450abd1a0454c588d37458db86154b36f041953
Thangmokub <98677394+Thangmokub@users.noreply.github.com>
2025-07-23 21:46:29 +0700

    Update main.py

--- main.py
+++ main.py
@@ -87,7 +87,7 @@ if not st.session_state.app_started:
 
     if st.button("เริ่มใช้งาน"):
         st.session_state.app_started = True
-        st.experimental_rerun()
+        
 
 # ========================
 # ส่วนหลักของแอป
